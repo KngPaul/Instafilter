2a12d88825b7c0d99f9eeea807b041b5cf86cd94
Paul Onawola <onawola.ire@gmail.com>
2023-09-09 22:28:57 +0100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Paul Onawola on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
